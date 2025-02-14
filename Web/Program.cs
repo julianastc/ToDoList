5e08163c2a50de60e80d550596f260b63d7cbc88
juliana <julianasantostav@gmail.com>
2025-02-14 15:10:42 -0300

    add migrations

---
+++ Web/Program.cs
@@ -0,0 +1,35 @@
+using Application.Services;
+using Infra;
+using Microsoft.EntityFrameworkCore;
+using Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+b => b.MigrationsAssembly("Infra")));
+builder.Services.AddScoped<IToDoServices, ToDoService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
